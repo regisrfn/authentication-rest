6806c46009b3e444880c77a8edbc9c928b774f40
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-04-09 16:15:10 -0300

    getting authorities and set up authentication

--- src/main/java/com/rufino/server/service/JwtTokenService.java
+++ src/main/java/com/rufino/server/service/JwtTokenService.java
@@ -1,6 +1,13 @@
 package com.rufino.server.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.rufino.server.model.User;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 public interface JwtTokenService {
 
@@ -9,5 +16,9 @@ public interface JwtTokenService {
     public boolean verifyToken(String token, String username);
 
     String getUsername(String token);
+
+    List<GrantedAuthority> getGrantedAuthorities(String token);
+
+    Authentication getAuthentication(String username, List<GrantedAuthority> authoritiesList,HttpServletRequest request);
 
 }
--- src/main/java/com/rufino/server/service/impl/JwtTokenServiceImpl.java
+++ src/main/java/com/rufino/server/service/impl/JwtTokenServiceImpl.java
@@ -1,5 +1,10 @@
 package com.rufino.server.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.rufino.server.domain.JwtToken;
@@ -9,6 +14,11 @@ import com.rufino.server.service.JwtTokenService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -44,6 +54,21 @@ public class JwtTokenServiceImpl implements JwtTokenService {
         } catch (JWTVerificationException e) {
             throw new ApiRequestException("Could not verify token", HttpStatus.FORBIDDEN);
         }
+    }
+
+    @Override
+    public List<GrantedAuthority> getGrantedAuthorities(String token) {
+        List<String> claims = jwt.getClaimsFromToken(token);
+        return claims.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authoritiesList,
+            HttpServletRequest request) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                    username, null, authoritiesList);
+            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            return usernamePasswordAuthenticationToken;
     }
 
 }

9eea1936ab6b0364c42d8d634c98ad51d1f821bf
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-04-12 10:45:01 -0300

    Refactoring and adding forbidden http status

--- src/main/java/com/rufino/server/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/rufino/server/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,8 @@
 package com.rufino.server.security;
 
+import static com.rufino.server.constant.SecurityConst.FORBIDDEN_MESSAGE;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -8,10 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.rufino.server.constant.SecurityConst;
 import com.rufino.server.domain.HttpResponse;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
@@ -27,9 +28,15 @@ public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
             throws IOException {
 
-        HttpResponse httpResponse = new HttpResponse(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN,
-                HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase(), SecurityConst.FORBIDDEN_MESSAGE);
+        HttpResponse httpResponse = new HttpResponse(
+            FORBIDDEN.value(), 
+            FORBIDDEN,
+            FORBIDDEN.getReasonPhrase().toUpperCase(), 
+            FORBIDDEN_MESSAGE);
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+
         OutputStream outputStream = response.getOutputStream();
         objectMapper.writeValue(outputStream, httpResponse);
         outputStream.flush();

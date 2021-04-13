64cb7c89da72adb7c247d1a358db30097b89368a
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-04-13 13:38:15 -0300

    add PreAuthorize annotation for delete

--- src/main/java/com/rufino/server/api/UserController.java
+++ src/main/java/com/rufino/server/api/UserController.java
@@ -11,6 +11,7 @@ import com.rufino.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -61,9 +62,11 @@ public class UserController {
     }
 
     @DeleteMapping("delete/{id}")
-    public Map<String, String> deleteUserById(@PathVariable String id) {
+    @PreAuthorize("hasAnyAuthority('DELETE')")
+    public ResponseEntity<Object> deleteUserById(@PathVariable String id) {
         userService.deleteUserById(id);
-        return Map.of("message", "successfully operation");
+        Map<String, String> message = Map.of("message", "successfully operation");
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @PutMapping("update")

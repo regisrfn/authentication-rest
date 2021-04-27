b7dee767ea74063beaa6edacbc6d2250836f629b
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-04-27 17:15:52 -0300

    impl delete method

--- src/main/java/com/rufino/server/service/impl/FileUploadServiceImpl.java
+++ src/main/java/com/rufino/server/service/impl/FileUploadServiceImpl.java
@@ -1,8 +1,13 @@
 package com.rufino.server.service.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.rufino.server.model.FileResponse;
 import com.rufino.server.service.FileUploadService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +27,7 @@ public class FileUploadServiceImpl implements FileUploadService{
     private RestTemplate restTemplate;
     private Dotenv dotEnv;
     private String apiUrl;
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public FileUploadServiceImpl(RestTemplate restTemplate, Dotenv dotEnv) {
@@ -38,8 +45,13 @@ public class FileUploadServiceImpl implements FileUploadService{
 
     @Override
     public void delete(String url) {
-        // TODO Auto-generated method stub
-        
+        List<String> split = Arrays.asList(url.split("/"));
+        try {
+            restTemplate.delete(apiUrl + "/delete/" + split.get(split.size() - 1)); 
+        } catch (RestClientException e) {
+            LOGGER.error(e.getMessage());
+        }
+               
     }
 
     private HttpEntity<MultiValueMap<String, Object>> createRequest(MultipartFile file) {
